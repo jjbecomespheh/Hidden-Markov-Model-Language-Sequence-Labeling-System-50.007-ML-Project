c044e6254bde8806b26fe7f8b19eb6242783bcf8
dylantzx <dyla>
2021-12-11 15:46:15 +0800

    changed variable names for part1

--- part1.py
+++ part1.py
@@ -1,10 +1,10 @@
-def est_em_params(emissions_count, states_count, x,y):
+def est_em_params(emissions_count, labels_count, x,y):
     
     count_yx = emissions_count[x][y] 
-    count_y = states_count[y]
+    count_y = labels_count[y]
     return count_yx/count_y
 
-def est_em_params_unk(emissions_count, states_count, x,y, k=1):
+def est_em_params_unk(emissions_count, labels_count, x,y, k=1):
 
     token= "#UNK#"
     
@@ -14,17 +14,17 @@ def est_em_params_unk(emissions_count, states_count, x,y, k=1):
         emissions_count[x][y] = emissions_count[x].get(y,0)
     
     if x == token:
-        return k/ (states_count[y]+k)
+        return k/ (labels_count[y]+k)
 
     else:
         count_yx= emissions_count[x][y] 
-        return count_yx/ (states_count[y]+k)
+        return count_yx/ (labels_count[y]+k)
 
-def calc_argmax(emissions_count, states_count, x):
+def calc_argmax(emissions_count, labels_count, x):
     
     em_prob = {}
-    for state in states_count:
-        em_prob[state] = est_em_params_unk(emissions_count, states_count, x, state)
+    for label in labels_count:
+        em_prob[label] = est_em_params_unk(emissions_count, labels_count, x, label)
 
     return max(em_prob, key=em_prob.get)
 
@@ -40,7 +40,7 @@ def save_to_file(output_file, line, tag):
 def train(path):
 
     emissions_count = {}
-    states_count = {"O": 0, "B-positive": 0, "I-positive": 0, "B-negative": 0, "I-negative": 0, "B-neutral": 0, "I-neutral": 0}
+    labels_count = {"O": 0, "B-positive": 0, "I-positive": 0, "B-negative": 0, "I-negative": 0, "B-neutral": 0, "I-neutral": 0}
 
     with open(path, "r") as f:
         lines_ls = f.readlines()
@@ -53,25 +53,26 @@ def train(path):
                 Store the observation and its count as a dictionary 
                 in the emissions_count dictionary 
 
-                Eg. For label 0 and word disfrutemos, 
-                {"0": {"disfrutemos: 1}} 
+                Eg. For label O and word disfrutemos, 
+                {"disfuremos": {"O": <count>, "B-positive": <count> , ...}} 
 
                 """
-                word, label = line.rsplit(" ",1)
+                word, lbl = line.rsplit(" ",1)
 
                 emissions_count[word] = emissions_count.get(word,{})
                 
-                for state in states_count:
-                    emissions_count[word][state] = emissions_count[word].get(state, 0)
+                # Populating the inner dictionary by initializing count to 0 for each label
+                for label in labels_count:
+                    emissions_count[word][label] = emissions_count[word].get(label, 0)
 
-                emissions_count[word][label] = emissions_count[word].get(label, 0) + 1
-                states_count[label] += 1
+                emissions_count[word][lbl] = emissions_count[word].get(lbl, 0) + 1
+                labels_count[lbl] += 1
 
             except:
                 # empty space
                 pass
             
-    return emissions_count, states_count
+    return emissions_count, labels_count
 
 if __name__ == "__main__":
     foldername = "ES"
@@ -81,7 +82,7 @@ if __name__ == "__main__":
     dev_path = f"{foldername}/dev.in"
     output_path = f"{foldername}/{output_file}"
 
-    emissions_count, states_count = train(train_path)
+    emissions_count, labels_count = train(train_path)
 
     with open(dev_path, "r") as f:
         open(output_path, "w")
@@ -91,7 +92,7 @@ if __name__ == "__main__":
             line = line.replace("\n", "")
 
             if line != "":
-                tag = calc_argmax(emissions_count, states_count, line)
+                tag = calc_argmax(emissions_count, labels_count, line)
             else:
                 tag = ""
             save_to_file(output_path, line, tag)
