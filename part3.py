257352d164282adf3577e458734f71e637d10eaf
hatsumikana <kana.nanomi@gmail.com>
2021-12-13 00:48:51 +0800

    Completed Part 3

--- part3.py
+++ part3.py
@@ -2,157 +2,104 @@ from part1 import *
 from part2 import *
 import pprint
 from math import log
+import itertools
+import ast
 
 def viterbi_5th_best(labels_transition_count, labels_emission_count, transition_count, emission_count, seq, k=1):
-     # STEP 1: Initialization
-    pi = { 0 : {"Start": [[1,[]]]} }
-    optimum_labels = []
+   
+    pp = pprint.PrettyPrinter(depth=6)
+    
+    # STEP 1: Initialization
+    pi = { 0 : {"[_, 'Start']": 0} }
 
     # STEP 2: Move Forward -> pi(j+1,u) = max{pi(j,v)*(b_u(x_j+1))*(a_v,u)}
     for j in range(1, len(seq)+1):
+
         pi[j] = {}
-        outer_ls = []
-        
+        temp_ls = []
         for v in labels_emission_count:
-            prob = {}
-            ls = []
-
-            for u in pi[j-1].keys(): #Find all prob from prev layer to node v -> Then get the max
+            for u in pi[j-1].keys():
+                #Find all prob from prev layer to node v -> Then get the max
+                new_u_pair = u.strip('][').split(', ')
+                # print(new_u_pair)
+                new_u = new_u_pair[-1][1:-1] # eg. remove "" from "'Start'"
+                # print(j, " -> ", u, " -> ", v)
+                temp_inner_ls = []
                 
-                """
-                j=1: u = "Start"
-
-                {1: {O: max_value, B-positive: max_value , ...}}
-
-                j=2: u = O, B-positive, ...
-
-                {2: {O: max_value, B-positive: max_value , ...}}
-
-                """
-
-                transition_prob = est_tr_params(transition_count, labels_transition_count, u, v)
+                transition_prob = est_tr_params(transition_count, labels_transition_count, new_u, v)
                 emission_prob = est_em_params_unk(emission_count, labels_emission_count, seq[j-1], v)
                 
+                for times in range(len(new_u_pair)):
+                    temp_inner_ls.append(new_u_pair[times][1:-1])
+                temp_inner_ls.append(v)
+
                 if transition_prob > 0 and emission_prob > 0:
                     # Use max log likelihood to resolve numerical underflow issue
-                    prob[u] = pi[j-1][u] + log(transition_prob) + log(emission_prob)
-                    for i in range(len(pi[j-1][u])):
-                        prob[tuple(pi[j-1][u][i][1]+[u])] = pi[j-1][u][i][0] \
-                        + log(transition_prob) \
-                        + log(emission_prob)
-
+                    temp_inner_ls.append(pi[j-1][u]  + log(transition_prob) + log(emission_prob))
                 else:
-                    for i in range(len(pi[j-1][u])):
-                        prob[tuple(pi[j-1][u][i][1]+[u])] = float("-inf")
+                    temp_inner_ls.append(float("-inf"))
 
-            probValues = sorted(prob.values(), reverse=True)
-            probKeys = sorted(prob, key=prob.get, reverse=True)
-            if len(probValues) == 1:
-                pi[j][v] = [[probValues[0], list(probKeys[0])]]
-            else:
-                pi[j][v] = [[probValues[0], list(probKeys[0])], [probValues[1], list(probKeys[1])], [probValues[2], list(probKeys[2])]]
+                temp_ls.append(temp_inner_ls)
+        # print("temp_ls ->")
+        # pp.pprint(temp_ls)
 
+            # pi[j][v] = prob[1]
+        temp_ls.sort(key = lambda x: -x[-1])
+        temp_ls = temp_ls[:5]
+        # pp.pprint(temp_ls)
 
+        for i in range(len(temp_ls)):
+            uv_ls = []
+            for time in range(len(temp_ls[i])-1):
+                uv_ls.append(temp_ls[i][time])
+            uv = str(uv_ls)
+            pi[j][uv] = temp_ls[i][-1]
 
-        pp = pprint.PrettyPrinter(depth=6)
-        pp.pprint(pi)
-    print(optimum_labels)     
-    # optimum_labels = None   
-    return optimum_labels
-
-def viterbi_5th_best_copy(labels_transition_count, labels_emission_count, transition_count, emission_count, seq, k=1):
-     # STEP 1: Initialization
-    pi = { 0 : {"Start": 0} }
-    optimum_labels = []
-
-    # STEP 2: Move Forward -> pi(j+1,u) = max{pi(j,v)*(b_u(x_j+1))*(a_v,u)}
-    for j in range(1, len(seq)+1):
-        pi[j] = {}
-        outer_ls = []
+    final_layer = pi[len(pi)-1]
+    fifth_best = 100000
+    for key, value in final_layer.items():
         
-        for v in labels_emission_count:
-            prob = {}
-            ls = []
+        if value < fifth_best:
+            fifth_best = value
 
-            for u in pi[j-1].keys(): #Find all prob from prev layer to node v -> Then get the max
-                
-                """
-                j=1: u = "Start"
+    final_labels = list(final_layer.keys())[list(final_layer.values()).index(fifth_best)]
+    final_labels = ast.literal_eval(final_labels)[2:]
+    return final_labels
 
-                {1: {O: max_value, B-positive: max_value , ...}}
-
-                j=2: u = O, B-positive, ...
-
-                {2: {O: max_value, B-positive: max_value , ...}}
-
-                """
-
-                transition_prob = est_tr_params(transition_count, labels_transition_count, u, v)
-                emission_prob = est_em_params_unk(emission_count, labels_emission_count, seq[j-1], v)
-                
-                if transition_prob > 0 and emission_prob > 0:
-                    # Use max log likelihood to resolve numerical underflow issue
-                    prob[u] = pi[j-1][u] + log(transition_prob) + log(emission_prob)
-                else:
-                    prob[u] = float("-inf")
-
-                ls.append(prob[u])
-
-            if j == 1:
-                pi[j][v] = prob[u]
-
-            else:
-                sorted_ls = sorted(ls)
-                print(sorted_ls)
-                fifth_best_utov = sorted_ls[-3]
-                pi[j][v] = fifth_best_utov
-            outer_ls.append(pi[j][v])
-            
-        sorted_outer_ls = sorted(outer_ls)
-        fifth_best_v = sorted_outer_ls[-3]
-        
-        for key, value in pi[j].items():
-            if value == fifth_best_v:
-                optimum_labels.append(key)
-                break
-
-        pp = pprint.PrettyPrinter(depth=6)
-        pp.pprint(pi)
-    print(optimum_labels)     
-    # optimum_labels = None   
-    return optimum_labels
 
 if __name__ == "__main__":
-    foldername = "ES"
-    output_file = "dev.p2.out"
+
+    foldername = "RU"
+    output_file = "dev.p3.out"
     
     train_path = f"{foldername}/train"
     dev_path = f"{foldername}/dev.in"
     output_path = f"{foldername}/{output_file}"
 
-    seq = ["La", "comida", "estuvo", "muy", "sabrosa", "."]
+    # seq = ["La", "comida", "estuvo", "muy", "sabrosa", "."]
     transition_count, labels_transition_count = count_transition(train_path)
-    emmision_count, labels_emmision_count = count_emission(train_path)
-    optimum_labels = viterbi_5th_best(labels_transition_count, labels_emmision_count, transition_count, emmision_count, seq)
-    
-    # with open(dev_path, "r") as f:
-    #     open(output_path, "w")
-        
-    #     lines_ls = f.readlines()
-        
-    #     temp_sentence = []
+    emission_count, labels_emission_count = count_emission(train_path)
 
-    #     for i in range(len(lines_ls)):
-    #         line = lines_ls[i].replace("\n", "")
+    # optimum_labels = viterbi_5th_best(labels_transition_count, labels_emission_count, transition_count, emission_count, seq)
+  
+    with open(dev_path, "r") as f:
+        open(output_path, "w")
+        
+        lines_ls = f.readlines()
+        
+        temp_sentence = []
+
+        for i in range(len(lines_ls)):
+            line = lines_ls[i].replace("\n", "")
             
-    #         if line != "":
-    #             temp_sentence.append(line)
-    #         else:
-    #             optimum_labels = viterbi(labels_transition_count, labels_emmision_count, transition_count, emmision_count, temp_sentence)
-    #             with open(output_path, "a") as f:
-    #                 for i in range(len(temp_sentence)):
-    #                     out = f"{temp_sentence[i]} {optimum_labels[i]}\n" 
-    #                     f.write(out)
-    #                 f.write('\n')
+            if line != "":
+                temp_sentence.append(line)
+            else:
+                optimum_labels = viterbi_5th_best(labels_transition_count, labels_emission_count, transition_count, emission_count, temp_sentence)
+                with open(output_path, "a") as f:
+                    for i in range(len(temp_sentence)):
+                        out = f"{temp_sentence[i]} {optimum_labels[i]}\n" 
+                        f.write(out)
+                    f.write('\n')
             
-    #             temp_sentence = []
+                temp_sentence = []
