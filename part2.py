24233190caaff98d37df3183ac24d505b67899bd
dylantzx <dyla>
2021-12-13 16:05:07 +0800

    Tidy up part2 code

--- part2.py
+++ part2.py
@@ -6,10 +6,20 @@ def est_tr_params(transition_count, labels_count, yi_1, yi):
     return transition_count[yi_1][yi]/labels_count[yi_1]
 
 def viterbi(labels_transition_count, labels_emission_count, transition_count, emission_count, seq, k=1):
+    
+    """
+    This approach is a modified version of the Viterbi algorithm to obtain the best sequence without backtracking.
+
+    This is done by looping through all the words in a sentence, all the possible labels for the words, then calculating
+    the probability of getting from nodes u to v.
+    
+    Values are stored in a dictionary to lower the time complexity in accessing the elements.
+
+    Maximum Log likelihood Estimation is used instead of Maximum Likelihood Estimation to prevent underflow issue.
+    """
     # STEP 1: Initialization
     pi = { 0 : {"Start": 0} }
     optimum_labels = []
-    pp = pprint.PrettyPrinter(depth=6)
 
     # STEP 2: Move Forward -> pi(j+1,u) = max{pi(j,v)*(b_u(x_j+1))*(a_v,u)}
     for j in range(1, len(seq)+1):
@@ -18,17 +28,6 @@ def viterbi(labels_transition_count, labels_emission_count, transition_count, em
             prob = {}
         
             for u in pi[j-1].keys(): #Find all prob from prev layer to node v -> Then get the max
-                
-                """
-                j=1: u = "Start"
-
-                {1: {O: max_value, B-positive: max_value , ...}}
-
-                j=2: u = O, B-positive, ...
-
-                {2: {O: max_value, B-positive: max_value , ...}}
-
-                """
 
                 transition_prob = est_tr_params(transition_count, labels_transition_count, u, v)
                 emission_prob = est_em_params_unk(emission_count, labels_emission_count, seq[j-1], v)
@@ -58,7 +57,6 @@ def count_transition(path):
                         "I-neutral": {"Start": 0, "O": 0, "B-positive": 0, "I-positive": 0, "B-negative": 0, "I-negative": 0, "B-neutral": 0, "I-neutral": 0, "Stop": 0},
                         }
     labels_count = {"Start": 0, "O": 0, "B-positive": 0, "I-positive": 0, "B-negative": 0, "I-negative": 0, "B-neutral": 0, "I-neutral": 0, "Stop": 0}
-    pp = pprint.PrettyPrinter(depth=6)
 
     with open(path, "r") as f:
         lines_ls = f.readlines()
@@ -97,23 +95,7 @@ def count_transition(path):
 
     return transition_count, labels_count
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--folder", "-f", type=str, default="./ES", help="Folder path")
-    parser.add_argument("--output", "-o", type=str, default="./ES/dev.p2.out", help="Output file path")
-
-    opt = parser.parse_args()
-
-    foldername = opt.folder
-    output_path = opt.output
-    
-    train_path = f"{foldername}/train"
-    dev_path = f"{foldername}/dev.in"
-
-    transition_count, labels_transition_count = count_transition(train_path)
-    emission_count, labels_emission_count = count_emission(train_path)
- 
+def save_output_to_file(dev_path, output_path, labels_transition_count, labels_emission_count, transition_count, emission_count):
     with open(dev_path, "r") as f:
         open(output_path, "w")
         
@@ -135,3 +117,21 @@ if __name__ == "__main__":
                     f.write('\n')
             
                 temp_sentence = []
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", "-f", type=str, default="ES", help="ES or RU")
+
+    opt = parser.parse_args()
+
+    foldername = opt.folder
+    
+    train_path = f"./{foldername}/train"
+    dev_path = f"./{foldername}/dev.in"
+    output_path = f"./{foldername}/dev.p2.out"
+
+    transition_count, labels_transition_count = count_transition(train_path)
+    emission_count, labels_emission_count = count_emission(train_path)
+ 
+    save_output_to_file(dev_path, output_path, labels_transition_count, labels_emission_count, transition_count, emission_count)
